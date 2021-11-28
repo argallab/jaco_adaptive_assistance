3e5843e698d534eaafbd462f5230f7bce74d6f4f
Deepak Gopinath <musicmagic@gmail.com>
2021-11-28 12:07:09 -0600

    trial gen script

--- src/simulators/scripts/generate_jaco_adaptive_trials.py
+++ src/simulators/scripts/generate_jaco_adaptive_trials.py
@@ -22,33 +22,65 @@ NUM_BLOCKS_PER_ALGO = 2
 TRIALS_PER_BLOCK_PER_ALGO = TRIALS_PER_ALGO / NUM_BLOCKS_PER_ALGO
 
 
-goals_for_scene = list(range(1, 5))
-goals_for_scene = 4*goals_for_scene
-print(goals_for_scene)
-home_positions = 4*['home_1'] + 4*['home_2'] + 4*['home_1'] + 4*['home_2']
-starting_modes = 2*MODES + random.sample(TRANS_START_MODES, 2) + random.sample(ROT_TRANS_MODES, 2)
-random.shuffle(starting_modes)
-print(starting_modes)
-all_combinations_for_scenelist(zip(goals_for_scene, home_positions, starting_modes)))
+
+home_positions_1 = 6*['home_1']
+goal_positions_1 = 3 * ['goal_3', 'goal_4']
+random.shuffle(goal_positions_1)
+print(goal_positions_1)
+starting_modes_1 = TRANS_START_MODES + ROT_TRANS_MODES
+random.shuffle(starting_modes_1)
+print(starting_modes_1)
+combo_h1 = list(zip(home_positions_1, goal_positions_1, starting_modes_1))
+
+home_positions_2 = 6*['home_2']
+goal_positions_2 = 3 * ['goal_1','goal_2']
+random.shuffle(goal_positions_2)
+print(goal_positions_2)
+starting_modes_2 = TRANS_START_MODES + ROT_TRANS_MODES
+random.shuffle(starting_modes_2)
+print(starting_modes_2)
+combo_h2 = list(zip(home_positions_2, goal_positions_2, starting_modes_2))
+
+print('Disamb Scene 1\n')
+#disamb block 1
+random_3_from_combo_h1 = random.sample(range(6), 3)
+random_3_from_combo_h2 = random.sample(range(6), 3)
+disamb_block_0 = [combo_h1[i] for i in random_3_from_combo_h1] + [combo_h2[i] for i in random_3_from_combo_h2]
+
+random.shuffle(disamb_block_0)
+print(disamb_block_0)
+
+print('Disamb Scene 2\n')
+#disamb block 2
+
+other_3_from_combo_h1 = [i for i in range(6) if i not in random_3_from_combo_h1]
+other_3_from_combo_h2 = [i for i in range(6) if i not in random_3_from_combo_h2]
+disamb_block_1 = [combo_h1[i] for i in other_3_from_combo_h1] + [combo_h2[i] for i in other_3_from_combo_h2]
+
+random.shuffle(disamb_block_1)
+print(disamb_block_1)
+
+print('Control Scene 1\n')
+
+#control block 1
+random_3_from_combo_h1 = random.sample(range(6), 3)
+random_3_from_combo_h2 = random.sample(range(6), 3)
+control_block_0 = [combo_h1[i] for i in random_3_from_combo_h1] + [combo_h2[i] for i in random_3_from_combo_h2]
+
+random.shuffle(control_block_0)
+print(control_block_0)
 
 
+#control block 2
+print('Control Scene 2\n')
+
+
+other_3_from_combo_h1 = [i for i in range(6) if i not in random_3_from_combo_h1]
+other_3_from_combo_h2 = [i for i in range(6) if i not in random_3_from_combo_h2]
+control_block_1 = [combo_h1[i] for i in other_3_from_combo_h1] + [combo_h2[i] for i in other_3_from_combo_h2]
+
+random.shuffle(control_block_1)
+print(control_block_1)
 
 def generate_experiment_trials(args):
     pass
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--trial_dir",
-        dest="trial_dir",
-        default=os.path.join(os.getcwd(), "trial_folders", "trial_dir"),
-        help="The directory where trials will be stored are",
-    )
-    parser.add_argument(
-        "--metadata_dir",
-        dest="metadata_dir",
-        default=os.path.join(os.getcwd(), "trial_folders", "metadata_dir"),
-        help="The directory where metadata of trials will be stored",
-    )
-    args = parser.parse_args()
-    generate_experiment_trials(args)
